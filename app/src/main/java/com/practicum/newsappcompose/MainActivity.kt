154208bca6b4f4c7a3977fb03e46f5a2db91ea9c
Vlad <tchervladislav@yandex.ru>
2023-12-23 17:03:59 +0700

    added system UI controller

--- app/src/main/java/com/practicum/newsappcompose/MainActivity.kt
+++ app/src/main/java/com/practicum/newsappcompose/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.practicum.newsappcompose.presentation.navgraph.NavGraph
 import com.practicum.newsappcompose.ui.theme.NewsAppComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -33,6 +37,17 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NewsAppComposeTheme {
+
+                val isSystemDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemDarkMode
+                    )
+                }
+
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
                     val startDestination = viewModel.startDestination
                     NavGraph(startDestination = startDestination)
