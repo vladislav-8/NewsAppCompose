b3fd160e229e0ef9a7484b36af58d417d876c123
Vlad <tchervladislav@yandex.ru>
2023-12-29 00:00:33 +0700

    added TopBar for DetailsScreen

---
+++ app/src/main/java/com/practicum/newsappcompose/presentation/details/DetailsScreen.kt
@@ -0,0 +1,8 @@
+package com.practicum.newsappcompose.presentation.details
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen() {
+
+}
---
+++ app/src/main/java/com/practicum/newsappcompose/presentation/details/DetailsViewModel.kt
@@ -0,0 +1,4 @@
+package com.practicum.newsappcompose.presentation.details
+
+class DetailsViewModel {
+}
